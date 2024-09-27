96e4959339c8e1b665f94892924afc40d3375872
ashushinde5309 <ashushinde5309@gmail.com>
2024-09-27 23:55:23 +0530

    Update Pattern5.java

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -6,9 +6,9 @@ public class Pattern5
 	public static void main(String[] args) 
 	{
 		char c='A';
-		for(int i=0;i<5;i++)
+		for(int i=0;i<4;i++)
 		{
-			for(int j=0;j<5;j++)
+			for(int j=0;j<4;j++)
 			{
 				System.out.print((char)(c+i+j)+" ");
 			}
